a96a3a637b349a8acbec06e44aea76fba1f66c0d
manekiyong <59855081+manekiyong@users.noreply.github.com>
2022-12-24 23:30:05 +0800

    merge mtcnn into face_id_api
    reduce the need for 1 additional container

--- face_id_api/src/mtcnn/manager.py
+++ face_id_api/src/mtcnn/manager.py
@@ -1,8 +1,11 @@
 import requests
+import io
 import base64
 import json
 import yaml
 
+from model.mtcnn import MTCNN
+from PIL import Image
 
 def read_yaml(file_path='config.yaml'):
     with open(file_path, "r") as f:
@@ -16,9 +19,36 @@ URL = config['MTCNN']['URL']
 class MTCNNManager():
 
     def __init__(self):
-        self.path = URL+'/crop'
-        self.header = {'Content-type': 'application/json',
-                       'Accept': 'text/plain'}
+        # self.path = URL+'/crop'
+        # self.header = {'Content-type': 'application/json',
+        #                'Accept': 'text/plain'}
+        self.mtcnn = MTCNN(image_size=160, margin=0, device='cuda:0', keep_all=True)
+
+    def parse_results(self, im_bytes):
+        image = Image.open(io.BytesIO(im_bytes)).convert('RGB')
+        img_tensor, prob, box, = self.mtcnn(image, return_prob=True)
+        res_dict = {}
+        if prob[0] == None:
+            res_dict['img'] = []
+            res_dict['prob'] = []
+            res_dict['box'] = []
+        else:
+            img_list = img_tensor.tolist()
+            prob_list = prob.tolist()
+            box_list = box.tolist()
+            pred_len = len(prob_list)
+            for i, conf in enumerate(reversed(prob_list)):
+                # Clear faces usually easily obtain conf of 0.99++
+                if conf < 0.9:  # Eliminate low confidence faces detected.
+                    pop_index = pred_len-i-1
+                    img_list.pop(pop_index)
+                    box_list.pop(pop_index)
+                    prob_list.pop(pop_index)
+            res_dict['img'] = img_list
+            res_dict['prob'] = prob_list
+            res_dict['box'] = box_list
+        return res_dict
+
 
     def crop_faces_from_path(self, img_path):
         """        
@@ -39,11 +69,12 @@ class MTCNNManager():
         """
         with open(img_path, "rb") as f:
             im_bytes = f.read()
-        im_b64 = base64.b64encode(im_bytes).decode("utf8")
-        payload = json.dumps({"image": im_b64})
-        r = requests.post(self.path, data=payload, headers=self.header)
-        res = json.loads(r.text)
-        return res
+        res_dict = self.parse_results(im_bytes)
+        # im_b64 = base64.b64encode(im_bytes).decode("utf8")
+        # payload = json.dumps({"image": im_b64})
+        # r = requests.post(self.path, data=payload, headers=self.header)
+        
+        return res_dict
 
     def crop_faces_from_b64(self, img_bytes):
         """        
@@ -64,7 +95,5 @@ class MTCNNManager():
                                 example shape: [N, 1]
                 where N is the number of faces found
         """
-        r = requests.post(self.path, data=json.dumps(
-            img_bytes), headers=self.header)
-        res = json.loads(r.text)
+        res = self.parse_results(img_bytes)
         return res
