2ce0bfe8499d01f7b1fe2a63248cab5df305a7e4
manekiyong <59855081+manekiyong@users.noreply.github.com>
2023-01-13 11:22:42 +0800

    faceid solely generates embedding

---
+++ face_id_api/src/embeddings/embedding.py
@@ -0,0 +1,39 @@
+import numpy as np
+import torch
+from typing import List
+
+from mtcnn.manager import MTCNNManager
+from triton.manager import TritonManager
+
+class EmbeddingGenerator():
+    def __init__(self):
+        self.mtcnn = MTCNNManager()
+        self.inference = TritonManager()
+
+    def generate_embedding(self, img_b64_list: List):
+        """
+        Takes in a list of images parsed as base64 format and generate the average embedding of all the faces
+        The assumption is that the image provided has only 1 distinct face within image to establish the identity of an individual
+
+        Args:
+            img_b64_list (list): Mapping to be checked
+
+        Returns:
+            int: 0 if there is invalid types, 1 otherwise
+
+        """
+        avg_emb = torch.zeros(512)
+        img_count = 0  # Reset Image Count
+        for i in img_b64_list:
+            face_data = self.mtcnn.crop_faces_from_b64(i)
+            if len(face_data['img']) != 0:
+                img_count += 1
+                max_val = np.argmax(face_data['prob'])
+                top_img = [face_data['img'][max_val]]
+                img_embedding = self.inference.infer_with_triton(top_img)
+                img_embedding = torch.from_numpy(img_embedding)
+                avg_emb = avg_emb.add(img_embedding[max_val])
+        if img_count == 0:
+            return []
+        avg_emb = avg_emb.div(img_count)
+        return avg_emb.tolist()
--- face_id_api/src/main.py
+++ face_id_api/src/main.py
@@ -1,26 +1,10 @@
-from embeddings.embeddings import EmbeddingDataset
-from embeddings.uploader import Uploader
-# from embeddings.identify import Identify
-
-import yaml
-from PIL import Image
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
 
+from embeddings.embedding import EmbeddingGenerator
 
-def read_yaml(file_path='config.yaml'):
-    with open(file_path, "r") as f:
-        return yaml.safe_load(f)
-
-
-config = read_yaml()
-
-EMBEDDING_PATH = config['EMB']['emb_path']
-
-
-dataset = EmbeddingDataset(EMBEDDING_PATH, local=False)
-uploader = Uploader(EMBEDDING_PATH, local=False)
+emb_generator = EmbeddingGenerator()
 
 
 class Image(BaseModel):
@@ -34,62 +18,14 @@ class ImageFolder(BaseModel):
 
 api = FastAPI()
 
-
-@api.post("/infer")
-def infer(img_data: Image):
-    """
-    Takes in an Image object and predicts the id of the face based
-    on existing list of embeddings; 
-    Returns the id, confidence and the corresponding bounding box. 
-    """
-    img_dict = img_data.dict()
-
-    all_cos_id, all_cos_conf, all_euc_id, all_euc_conf, bb, all_embs = dataset.find_id(
-        img_dict)
-    if config['EMB']['get_emb']:
-        ret_dict = {
-            "cos_id": all_cos_id,
-            "cos_conf": all_cos_conf,
-            "euc_id": all_euc_id,
-            "euc_conf": all_euc_conf,
-            "bb": bb,
-            "emb": all_embs
-        }
-    else:
-        ret_dict = {
-            "cos_id": all_cos_id,
-            "cos_conf": all_cos_conf,
-            "euc_id": all_euc_id,
-            "euc_conf": all_euc_conf,
-            "bb": bb
-        }
-    return ret_dict
-
-
-@api.post("/generate")
-def generate(img_list: ImageFolder):
+@api.post("/embedding")
+def embedding(img_list: ImageFolder):
     """
     Takes in an ImageFolder object, generates an embedding and saves it locally.
     """
     img_dict = img_list.dict()
-    emb = dataset.generate_embedding_from_b64(
-        img_dict['id'], img_dict['images'])
+    emb = emb_generator.generate_embedding(img_dict['images'])
     if emb == []:
-        return {"Response": "No embedding generated"}
+        return {"response": "No embedding generated"}
     else:
-        uploader.save_emb(img_dict['id'], emb)
-        return {"Response": "OK"}
-
-# @api.post("/embedding")
-# def generate(img_list: ImageFolder):
-#     """
-#     Takes in an ImageFolder object, generates an embedding and saves it locally.
-#     """
-#     img_dict = img_list.dict()
-#     emb = dataset.generate_embedding_from_b64(
-#         img_dict['id'], img_dict['images'])
-#     if emb == []:
-#         return {"Response": "No embedding generated"}
-#     else:
-#         uploader.save_emb(img_dict['id'], emb)
-#         return {"Response": "OK"}
+        return {"response": "200", "emb": emb}
